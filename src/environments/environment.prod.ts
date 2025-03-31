15f432084ee3a6b11cf0280224a043664ae2a9c9
carlos de silvestri ruiz <carlosdesilvestrir@javeriana.edu.co>
2025-03-30 22:39:01 -0500

    Hofix: enviroment variables problem.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    profileapiBaseUrl: 'apigateway-service-production.up.railway.app/profile/',
+    recipeBaseUrl: 'apigateway-service-production.up.railway.app/recipe/',
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    recipeBaseUrl: 'http://localhost:8001',
+    profileapiBaseUrl: 'http://localhost:8000',
+};
