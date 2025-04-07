fd25e1fa545530867ae1dfd370762e5879a2494a
Tomas Martinez <105027021+iTMG22@users.noreply.github.com>
2025-04-07 15:51:37 -0500

    Update server.js
    Update Cors

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require("express");
-
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 const DIST_FOLDER = path.join(__dirname, "dist/web-application/browser");
 
+app.use(cors({
+  origin: 'https://webproject-production-8adc.up.railway.app',
+  credentials: true
+}));
+
 app.use(express.static(DIST_FOLDER));
 
 app.get("*", (req, res) => {
-
   res.sendFile(path.join(DIST_FOLDER, "index.csr.html"));
-
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-
   console.log(`Servidor corriendo en el puerto ${PORT}`);
-
-}); 
+});
