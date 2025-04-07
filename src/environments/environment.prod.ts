00f0ba821879e19446f3f0571e916c34506fe104
Tomas Martinez <105027021+iTMG22@users.noreply.github.com>
2025-04-06 21:42:20 -0500

    Update environment.prod.ts
    Se modifico el archivo enviromment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    profileapiBaseUrl: 'https://apigateway-service-production.up.railway.app/profile',
-    recipeBaseUrl: 'https://apigateway-service-production.up.railway.app/recipe',
-    authBaseUrl: 'authentication-service-production-af27.up.railway.app/login',
-    reportBaseUrl: 'reports-service-production.up.railway.app'
+    profileapiBaseUrl:'https://apigateway-service-production.up.railway.app/profile',
+    recipeBaseUrl:'https://apigateway-service-production.up.railway.app/recipe',
+    authBaseUrl:'https://authentication-service-production-af27.up.railway.app/login',
+    reportBaseUrl:'https://reports-service-production.up.railway.app/report'
   };
   
