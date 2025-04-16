e4e723d56cc66fcc939a29e6f5cb90a757755877
Tomas Martinez <105027021+iTMG22@users.noreply.github.com>
2025-04-16 18:42:16 -0500

    Update auth.guard.ts
    Change Token to Acces_token

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -8,7 +8,7 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('access_token');
     if (!token) {
       this.router.navigate(['/login']);
       return false;
